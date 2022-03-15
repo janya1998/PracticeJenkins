fba5aa329648d36d3614c50fb6114a4428986f92
soujanya <ssoujanya998@gmail.com>
2022-03-15 13:07:42 +0530

    commit

---
+++ src/test/java/CreateContactTest/CreateContactTest.java
@@ -0,0 +1,17 @@
+package CreateContactTest;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	@Test
+	public void createcontest() {
+		System.out.println("contact created");
+	}
+
+
+@Test
+public void createcontest1() {
+	System.out.println("contact again created");
+}
+
+}
---
+++ src/test/java/CreateOrgTest/CreateOrgTest.java
@@ -0,0 +1,13 @@
+package CreateOrgTest;
+
+import org.testng.annotations.Test;
+
+
+public class CreateOrgTest {
+	@Test
+	public void orgTest() {
+		System.out.println("Org Created");
+	}
+	
+
+}
