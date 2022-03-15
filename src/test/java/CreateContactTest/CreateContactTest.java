c306d636dc824f4b6df899fb0f2349fbd6f7e44a
soujanya <ssoujanya998@gmail.com>
2022-03-15 16:17:13 +0530

    my new commit

--- src/test/java/CreateContactTest/CreateContactTest.java
+++ src/test/java/CreateContactTest/CreateContactTest.java
@@ -4,15 +4,9 @@ import org.testng.annotations.Test;
 
 public class CreateContactTest {
 	@Test
-	public void createcontest() {
+	public void createcontest1() {
 		System.out.println("contact created");
 	}
-
-
-@Test
-public void createcontest1() {
-	System.out.println("contact again created");
-}
 
 
 @Test
@@ -20,4 +14,15 @@ public void createcontest2() {
 	System.out.println("contact again created");
 }
 
+
+@Test
+public void createcontest3() {
+	System.out.println("contact again created");
+
+	
+}
+@Test
+public void createcontest4() {
+	System.out.println("contact again created");
+}
 }
