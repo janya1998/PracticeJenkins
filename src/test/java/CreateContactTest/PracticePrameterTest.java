105c3310a8689c1a916da7d13a82e7b974e40e2f
soujanya <ssoujanya998@gmail.com>
2022-03-16 09:21:03 +0530

    THIS MY NEW COMMIT

---
+++ src/test/java/CreateContactTest/PracticePrameterTest.java
@@ -0,0 +1,15 @@
+package CreateContactTest;
+
+import org.testng.annotations.Test;
+
+public class PracticePrameterTest {
+	@Test
+	public void Practice() {
+		String BROWSER = System.getProperty("browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		
+	}
+
+}
