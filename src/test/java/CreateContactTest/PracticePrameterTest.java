414daf637460364afea1be4512e2d32b12e0711b
soujanya <ssoujanya998@gmail.com>
2022-03-16 10:11:39 +0530

    new commit

--- src/test/java/CreateContactTest/PracticePrameterTest.java
+++ src/test/java/CreateContactTest/PracticePrameterTest.java
@@ -7,6 +7,8 @@ public class PracticePrameterTest {
 	public void Practice() {
 		String BROWSER = System.getProperty("browser");
 		System.out.println(BROWSER);
+		
+		
 		String URL = System.getProperty("url");
 		System.out.println(URL);
 		
